4e70f58f732a9f7b39f8116a53b92aec20a999c0
Vaishnavi <vaishuj7770@gmail.com>
2026-05-15 16:46:28 +0530

    Elif statement example

---
+++ DAY3/condition1.py
@@ -0,0 +1,16 @@
+#elif statement
+
+marks = 85
+
+if marks<60:
+    print("Grade: D")
+elif marks<70:
+    print("Grade: C")
+elif marks<80:
+    print("Grade: B")
+elif marks<90:
+    print("Grade: A")
+elif marks<=100:
+    print("Grade: A+")
+else:
+    print("Grade: F")
