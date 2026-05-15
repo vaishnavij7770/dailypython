a065809f23b9edf22cefcc1e07dbe6a4422061e1
Vaishnavi <vaishuj7770@gmail.com>
2026-05-15 17:42:41 +0530

    For loop examples

--- DAY3/condition2.py
+++ DAY3/condition2.py
@@ -9,4 +9,33 @@ if n>0:
     else:
         print("n is odd")
 else:
-    print("n is negative")
+    print("n is negative or zero")
+
+age = int(input("Enter age: "))
+
+if age >= 18:
+    if age >= 60:
+        print("Senior Citizen")
+    else:
+        print("Adult")
+else:
+    print("Minor")
+
+password = input("Enter password: ")
+
+if len(password) >= 8:
+    if password== "password123":
+        print("Password is correct")
+    else:
+        print("Password is incorrect")
+else:
+    print("Password is too short")
+
+marks=78
+if marks>=90:
+    if marks==100:
+        print("Grade: A+")
+    else:
+        print("Grade: A")
+else:
+    print("Grade: B or below")
---
+++ DAY3/forloop.py
@@ -0,0 +1,16 @@
+# for i in range(0,5):
+#     print("vaishnavi",end=" ")
+#     print(i)
+
+# for j in range(10,0,-2):
+#     print(j)
+
+# for k in range(1,11):
+#     print(k)
+
+# for l in range(5):
+#     print(l)
+
+for m in range(10,0,-2):
+    print(m)
+
