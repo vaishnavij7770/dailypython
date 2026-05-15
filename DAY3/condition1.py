814a94eb0b72d6994ae724998aeac901475e1e52
Vaishnavi <vaishuj7770@gmail.com>
2026-05-15 16:51:37 +0530

    Update condition1.py

--- DAY3/condition1.py
+++ DAY3/condition1.py
@@ -14,3 +14,15 @@ elif marks<=100:
     print("Grade: A+")
 else:
     print("Grade: F")
+
+age = 25
+if age<13:
+    print("Child")
+elif age<20:
+    print("Teenager")
+elif age<65:
+    print("Adult")
+else:
+    print("Senior")
+
+
