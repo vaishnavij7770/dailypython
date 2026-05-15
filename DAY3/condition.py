e2e749ee91d57a6a03899edf93e48160f58483b2
Vaishnavi <vaishuj7770@gmail.com>
2026-05-15 16:35:28 +0530

    Update condition.py

--- DAY3/condition.py
+++ DAY3/condition.py
@@ -10,3 +10,21 @@ if b > 0:
     print("b is positive")
 else:
     print("b is negative")
+
+c= 18
+if c >= 18:
+    print("c is an adult")
+else:
+    print("c is a minor")
+
+d= 5
+if d % 5 == 0:
+    print("d is a multiple of 5")
+else:
+    print("d is not a multiple of 5")
+
+e= 15
+if e > 10 and e < 20:
+    print("e is between 10 and 20")
+else:
+    print("e is not between 10 and 20")
