2520255dab47390d7462fd2a79ba4e4bf828e000
Vaishnavi <vaishuj7770@gmail.com>
2026-05-19 12:42:29 +0530

    Update method.py

--- DAY4/method.py
+++ DAY4/method.py
@@ -1,15 +1,24 @@
 l1=["python","java","c++","javascript"]
 l2=["js","html","css"]
 
-l1.append("ruby")
-print(l1)
+# l1.append("ruby")
+# print(l1)
 
-l1.append(l2) 
-print(l1)
+# l1.append(l2) 
+# print(l1)
 
-l1.clear()
-print(l1)
+# l1.clear()
+# print(l1)
 
-l3=l2.copy()
-print(l3)
+# l3=l2.copy()
+# print(l3)
+
+# cnt=l2.count("js")
+# print(cnt)
+
+cn=l1.count("java")
+print(cn)
+
+ind=l1.index("c++")
+print(ind)
 
