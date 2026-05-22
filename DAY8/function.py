2c45e320eb77c36abf78ea19a616d0e3deb674fe
Vaishnavi <vaishuj7770@gmail.com>
2026-05-22 16:30:59 +0530

    Create function.py

---
+++ DAY8/function.py
@@ -0,0 +1,16 @@
+def hello():
+    print("hello this is mansoon")
+
+hello()
+
+def add():
+    a=4
+    b=6
+    print(a+b)
+add()
+
+def abc(phrase):
+    print(phrase)
+
+
+abc("mansoon coming soon")
