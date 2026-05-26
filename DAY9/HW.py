776b45f5d8b06c79a0e7106d57c2424cc46d1527
Vaishnavi <vaishuj7770@gmail.com>
2026-05-26 11:54:01 +0530

    Update HW.py

--- DAY9/HW.py
+++ DAY9/HW.py
@@ -17,17 +17,67 @@ def square(s):
 
 print(square(6))
 
-def is_even(n):
+def even(n):
     return n % 2 == 0
 
-print(is_even(10))   
-print(is_even(7))
+print(even(10))   
+print(even(7))
 
-def check_number(n):
+def number(n):
     if n >= 0:
         return "Positive"
     else:
         return "Negative"
 
-print(check_number(5))
-print(check_number(-3))
+print(number(5))
+print(number(-3))
+
+
+# 5 example programs of recursion
+
+# sum of first n natural number
+
+def sum(n):
+    if n == 0:
+        return 0
+    return n + sum(n - 1)
+
+print(sum(4))
+
+# count from  N to 1 
+
+def countdown(n):
+    if n == 0:
+        return
+    print(n)
+    countdown(n - 1)
+
+countdown(5)
+
+# reverse string
+
+def reverse(s):
+    if len(s) == 0:
+        return ""
+    return reverse(s[1:]) + s[0]
+
+print(reverse("vaishu")) 
+
+#length string
+
+def length(s):
+    if s == "":
+        return 0
+    return 1 + length(s[1:])
+
+print(length("Python"))
+
+#print seperate char
+
+def char(s):
+    if len(s) == 0:
+        return
+    print(s[0])
+    char(s[1:])
+
+char("vaishu")
