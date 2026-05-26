6fdffd230b546ba9f7ca29299709d5398d8cef31
Vaishnavi <vaishuj7770@gmail.com>
2026-05-26 17:00:34 +0530

    Create array.py

---
+++ DAY10/array.py
@@ -0,0 +1,26 @@
+#three types of module //.py file
+#user defined module
+#built in module
+# third party module
+
+#array - import - use
+#numpy - install - import - use
+
+
+#following 3 ways to define array in python or import
+
+#import array as arr
+
+#from array import array
+
+from array import *
+
+ar1=array('i',[2,3,4,5])
+print(ar1)
+print(type(ar1))
+
+import array as arr
+
+ar1=arr.array('i',[2,3,4,5])
+print(ar1)
+print(type(ar1))
