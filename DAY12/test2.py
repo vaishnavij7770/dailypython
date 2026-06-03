c38e07032379247033e2de147220cbc4354f885f
Vaishnavi <vaishuj7770@gmail.com>
2026-06-03 11:55:44 +0530

    Module Updated

---
+++ DAY12/test2.py
@@ -0,0 +1,5 @@
+def t2():
+    print("I am test2")
+
+def t3():
+    print("I am test3")
--- DAY12/website.py
+++ DAY12/website.py
@@ -1,4 +1,6 @@
-import test1
+import test1,test2
 
 print("welcome to python")
 test1.t1()
+test2.t2()
+test2.t3()
