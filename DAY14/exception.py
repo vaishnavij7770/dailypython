fc96f6f6ba9858847e5a6a1dd6aec9f6b0b033bb
Vaishnavi <vaishuj7770@gmail.com>
2026-06-03 14:39:50 +0530

    Create exception.py

---
+++ DAY14/exception.py
@@ -0,0 +1,36 @@
+# try:
+#     a=int(input("Enter 1st number:"))
+#     b=int(input("Enter 2nd Number:"))
+
+
+#     c=a/b       #4/0
+#     print(c)    #exception handle
+# except:
+#     print("Something went wrong")
+#     print("Please check your input and try again")
+
+#     print("program executed successfully")
+
+
+
+
+try:
+    a=int(input("Enter 1st number:"))
+    b=int(input("Enter 2nd Number:"))
+
+
+    c=a/b       #4/0
+    print(c)    #exception handle
+
+except NameError as n:
+    print(n)
+
+except IndentationError as i:
+    print(i)
+
+except ZeroDivisionError as z:
+    print(z)
+
+except Exception as e:
+
+    print(e)
