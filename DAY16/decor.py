eeb4126895157d7453d2d43fc7a0bcead11bece9
Vaishnavi <vaishuj7770@gmail.com>
2026-06-04 16:41:22 +0530

    Update decor.py

--- DAY16/decor.py
+++ DAY16/decor.py
@@ -4,3 +4,17 @@ def domain():
 domain()
 
 base = domain()
+
+def outer_fun():
+    print("This is outer function")
+
+    def inner_fun1():
+        print("This is inner1 function")
+
+    def inner_fun2():
+        print("This is inner2 function")
+    
+    inner_fun1()
+    inner_fun2()
+
+outer_fun()
