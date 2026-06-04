503328d90d3011ae9ddfa2b1e8f083b020aa8024
Vaishnavi <vaishuj7770@gmail.com>
2026-06-04 16:51:56 +0530

    Update decor.py

--- DAY16/decor.py
+++ DAY16/decor.py
@@ -18,3 +18,39 @@ def outer_fun():
     inner_fun2()
 
 outer_fun()
+
+def greet():
+    print("Hello, Python learner!")
+
+greet()
+
+msg = greet()
+
+
+def calculator():
+    print("This is the calculator function")
+    print("num1=5, num2=3")
+
+
+    def add(a, b):
+        print("Addition:=", a + b)
+
+    def subtract(a, b):
+        print("Subtraction:=", a - b)
+
+    def multiply(a, b):
+        print("Multiplication: =", a * b)
+
+    def divide(a, b):
+        print("Division: =", a / b)
+
+    add(5, 3)
+    multiply(5, 3)
+    subtract(5, 3)
+    divide(5, 3)
+
+calculator()
+
+
+
+
