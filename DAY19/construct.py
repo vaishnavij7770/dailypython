940006e0249461a9b52ceb4e3f24ee251d1dfa5d
Vaishnavi <vaishuj7770@gmail.com>
2026-06-10 16:35:10 +0530

    Update construct.py

--- DAY19/construct.py
+++ DAY19/construct.py
@@ -9,3 +9,18 @@ class abc:
 a = abc()       #constructor will be called automatically when we create an object of the class
 a.show()    
 # a.__init__()
+
+#parameterized constructor
+class xyz:
+    id=0
+    name=""
+
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name
+        print(id,name)
+
+b = xyz(101,"abc")
+print(b.id)
+print(b.name)
+ 
