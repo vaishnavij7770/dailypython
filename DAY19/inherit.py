fcf9fb5281f6e356d31dc92215d37383db6a320b
Vaishnavi <vaishuj7770@gmail.com>
2026-06-11 16:38:00 +0530

    Update inherit.py

--- DAY19/inherit.py
+++ DAY19/inherit.py
@@ -1,3 +1,5 @@
+#single inheritance
+
 class Parent:
     print("Parent class")
     
@@ -10,3 +12,50 @@ class Child(Parent):
 c = Child()
 c.display()
 
+#multilevel inheritance
+
+class GrandParent:
+    print("GrandParent class")
+    
+class Parent2(GrandParent):
+    print("Parent class")
+
+class Child2(Parent2):
+    print("Child class")
+
+
+c = Child2()
+
+#multiple inheritance
+class Father:
+    print("Father class")
+
+class Mother:
+    print("Mother class")
+
+class Child3(Father, Mother):
+    print("Child class")
+
+c = Child3()
+
+#hierarchical inheritance
+class Parent3:
+    print("Parent class")
+class Child4(Parent3):
+    print("Child1 class")
+class Child5(Parent3):
+    print("Child2 class")
+
+c4 = Child4()
+c5 = Child5()
+
+#hybrid inheritance
+class A:
+    print("Class A")
+class B:
+    print("Class B")
+class C(A, B):
+    print("Class C")
+class D(C):
+    print("Class D")
+d = D()
