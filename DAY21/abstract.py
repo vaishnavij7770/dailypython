611e1f3c1216036238a3542fb116f204192299d6
Vaishnavi <vaishuj7770@gmail.com>
2026-06-15 16:46:02 +0530

    Update abstract.py

--- DAY21/abstract.py
+++ DAY21/abstract.py
@@ -1,23 +1,44 @@
 #abstraction in python
 
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 
-class polygon(ABC):
-    @abstractmethod
-    def sides(self):
-        pass
+# class polygon(ABC):
+#     @abstractmethod
+#     def sides(self):
+#         pass
 
-class triangle(polygon):
-    def sides(self):
-        print("Triangle have 3 sides")
+# class triangle(polygon):
+#     def sides(self):
+#         print("Triangle have 3 sides")
 
-class square(polygon):
-    def sides(self):
-        print("square have 4 sides")
+# class square(polygon):
+#     def sides(self):
+#         print("square have 4 sides")
 
-t = triangle()
-t.sides()
+# t = triangle()
+# t.sides()
 
-s = square()
-s.sides()
+# s = square()
+# s.sides()
         
+
+from abc import ABC, abstractmethod
+class vehicle(ABC):
+    @abstractmethod
+    def milege(self):
+        print("milge given")
+
+class car(vehicle):
+    def milege(self):
+        super().milege()
+        print("16")
+
+class scooter(vehicle):
+    def milege(self):
+        print("30")
+
+c = car()
+c.milege()
+
+s = scooter()
+s.milege()
